4002ea6acbff470901f73add5e2eaffb41000a00
Maksim Kreminskii <mail@kjmax.com>
2024-01-17 21:37:29 +0000

    API Client

---
+++ app/Services/AlphaVantageService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class AlphaVantageService
+{
+    const API_FUNCTION = 'TIME_SERIES_INTRADAY';
+    const TIME_KEY = 'Time Series (1min)';
+
+    protected $baseUrl = 'https://www.alphavantage.co/query';
+    protected $apiKey;
+
+    public function __construct()
+    {
+        $this->baseUrl = config('services.alpha_vantage.base_url');
+        $this->apiKey = config('services.alpha_vantage.api_key');
+    }
+
+    public function fetchStockData($symbol, $interval = '1min')
+    {
+        $response = Http::get($this->baseUrl, [
+            'function' => self::API_FUNCTION,
+            'symbol' => $symbol,
+            'interval' => $interval,
+            'apikey' => $this->apiKey
+        ]);
+
+        $stockPrices = [];
+
+        if ($response->successful()) {
+            foreach ($response[self::TIME_KEY] as $dateTime => $values) {
+                $stockPrices[] = [
+                    'symbol' => $symbol,
+                    'date_time' => $dateTime,
+                    'price' => $values['4. close']
+                ];
+            }
+        }
+
+        // TODO: Error handling
+
+        return $stockPrices;
+    }
+}
