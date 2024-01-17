1a452d77fba9d1c1117302f423d3b8f9763aa4be
Maksim Kreminskii <mail@kjmax.com>
2024-01-17 21:36:53 +0000

    DB migration

---
+++ database/migrations/2024_01_17_195303_create_stock_prices_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Stock;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_prices', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('symbol', 8);
+            $table->decimal('price', 10, 2);
+            $table->dateTime('date_time');
+
+            $table->unique(['symbol', 'date_time']);
+            $table->index(['symbol']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_prices');
+    }
+};
